ad334cf953b5bef7816dfa35dda7e2edfcc8bdeb
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-03-11 15:00:14 +0100

    Improve parser for multiple companies

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"gostockscraper/scraper"
 )
 
@@ -12,12 +13,24 @@ func main() {
 	flag.StringVar(&file, "f", "", "File with company symbols to be sent to the scraper")
 	flag.Parse()
 
+	rest := flag.Args()
+
+	companies := []string{company}
+	companies = append(companies, company)
+	companies = append(companies, rest...)
+
+	fmt.Printf("Company: %s\n", company)
+	fmt.Printf("File: %s\n", file)
+	fmt.Printf("Tail: %s\n", flag.Args())
+
 	scraper := scraper.New()
 
 	// TODO
 	// Improve this provisional splitting
 	if len(company) > 0 {
-		scraper.Scrap(company, false)
+		for _, c := range companies {
+			scraper.Scrap(c, false)
+		}
 	}
 	if len(file) > 0 {
 		scraper.Scrap(file, true)
